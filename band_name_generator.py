e296ab0d7467b866f50e3953507beb23392f03cd
Harish Sundaram <47561553+harishsun@users.noreply.github.com>
2023-11-26 14:15:14 +0000

    added the code to the file

---
+++ band_name_generator.py
@@ -0,0 +1,8 @@
+# Welcome message
+print("Welcome to the Band Name Generator!")
+# Asking user for the city and store it in a variable city_name
+city_name = input("what is the name of the city you grew up in? \n")
+# Asking user for the pet name and store it in the variable pet_name
+pet_name = input("what is the name of your pet? \n")
+# Print the band name by concatinating the two variables
+print("Your band name could be " + city_name + " " + pet_name)
