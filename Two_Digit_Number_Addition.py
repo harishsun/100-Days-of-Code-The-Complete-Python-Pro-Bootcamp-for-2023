763014f88ec3d76dd181839ff2216e0cf65e7817
Harish Sundaram <47561553+harishsun@users.noreply.github.com>
2023-11-27 09:29:27 +0000

    Two_digit_addition

---
+++ Two_Digit_Number_Addition.py
@@ -0,0 +1,11 @@
+two_digit_number = input()
+#convert the input to string
+str_input = str(two_digit_number)
+#Find the first string and convert it into Integer
+first_digit = int(str_input[0])
+#print(first_digit)
+#find the second digit and covert it into Integer
+second_digit = int(str_input[1])
+#print(second_digit)
+#print the addition
+print(first_digit + second_digit)
